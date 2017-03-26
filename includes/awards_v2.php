3d7c7b7cc14a539d86e2023f44476d0e52c20c50
Andy Knudsen <andy@websitetalkingheads.com>
2017-03-26 03:23:50 -0600

    got everying up to A0.2

---
+++ includes/awards_v2.php
@@ -0,0 +1,18 @@
+<section class="container">
+    <div class="row text-center awards">
+        <h2><span class="thin">Our </span> Awards</h2>
+        <h4>Website Talking Heads has earned numerous Awards over the years.</h4>
+        <h4>Including an A+ Rating from the Better Business Bureau</h4>
+        <div class="col-md-3">
+            <a href="#" data-toggle="tooltip" title="#1 in Video Production"> <img class="img img-responsive seal" src="http://websitetalkingheads.com/images/video-production-seal.png"  alt="Best in Video Production" /> </a>
+            <h4 class="text-center no-margin">#1 Video<br />Production</h4>
+        </div>
+        <div class="col-md-6"> <a href="#" data-toggle="tooltip" title="Winner Best Web Design Agencies - Video Production"><img class="img img-responsive seal"  alt="Winner in Video Production" src="http://www.websitetalkingheads.com/images/video-production-winner.png"></a>
+            <h4 class="text-center no-margin">Best Web Design Agencies<br />Video Production</h4>
+        </div>
+        <div class="col-md-3">
+            <a href="http://www.topseos.com/rankings-of-best-virtual-spokesperson-companies" data-toggle="tooltip" title="#1 in Video Spokesperson"><img class="img img-responsive seal" src="http://websitetalkingheads.com/images/video-spokesperson-seal.png"  alt="Best in Video Spokesperson"  /></a>
+            <h4 class="text-center no-margin">#1 Video<br />Spokesperson</h4>
+        </div>
+    </div>
+</section>
